47f08b251a6ec6597486571c043423710372016b
alex (is a badger, an IRL badger) <73854333+alexisabadger@users.noreply.github.com>
2022-05-17 19:19:21 -0400

    not an empty commit

--- cors/index.js
+++ cors/index.js
@@ -1,8 +1,15 @@
 const cors = require('cors');
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://alexmcintire.herokuapp.com/',
     optionsSuccessStatus: 200 // For legacy browser support
 }
 
 module.exports = cors(corsOptions);
+
+
+// localhost cors settings
+// const corsOptions = {
+//    origin: 'http://localhost:3000',
+//    optionsSuccessStatus: 200 // For legacy browser support
+//}
